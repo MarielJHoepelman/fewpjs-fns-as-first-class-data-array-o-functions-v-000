1af679f55f660c21f5677ce246fbbc8ed6847945
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-07-12 17:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function wakeDog(name,breed) {
   console.log(`Wake ${name} the ${breed}`);
 }
 
-function leashDog() {
-  
+function leashDog(name, breed) {
+  console.log(`Leash ${name} the ${breed}`);
 }
